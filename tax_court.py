4db1cd106062fb29cb1040d058c097c1f3f48131
andreas.dewantara@gmail.com <andreas.dewantara@gmail.com>
2021-02-23 17:24:02 +0700

    Change Gaussian Naive Bayes into Complement

--- tax_court.py
+++ tax_court.py
@@ -5,11 +5,11 @@ from sklearn.metrics import f1_score, classification_report, confusion_matrix, a
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import TruncatedSVD
+from sklearn.naive_bayes import ComplementNB
 
 #Prep
 df = pd.read_csv(r'D:\Blag - DATA\tax_court_stem_2.csv',header=0, converters={'no_putusan':str,'jenis_pajak':str,'sengketa':str,'djp_arg':str,'wp_arg':str,'pdpt_majelis':str})
 df = df.apply(lambda x: x.astype(str).str.lower())
-
 
 features_djp = df['djp_arg'].values
 features_wp = df['wp_arg'].values
@@ -26,10 +26,11 @@ processed_features_maj = vectorizer.fit_transform(features_maj).toarray()
 svd = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
 processed_svd = svd.fit_transform(processed_features_maj)
 
+#reguler random forest, SVM, and Naive Bayes
 X_train1, X_test1, y_train1, y_test1 = train_test_split(processed_features_maj, labels, test_size=0.2, random_state=1, stratify=labels)
-text_classifier = RandomForestClassifier(n_estimators=200, random_state=0)
-text_classifier.fit(X_train1, y_train1)
-predictions = text_classifier.predict(X_test1)
+rf = RandomForestClassifier(n_estimators=200, random_state=0)
+rf.fit(X_train1, y_train1)
+predictions = rf.predict(X_test1)
 print(confusion_matrix(y_test1,predictions))
 print(classification_report(y_test1,predictions))
 print(accuracy_score(y_test1, predictions))
@@ -38,14 +39,19 @@ X_train2, X_test2, y_train2, y_test2 = train_test_split(processed_features_maj, 
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(X_train2, y_train2)
 y_pred2 = svclassifier.predict(X_test2)
-
 print(f1_score(y_test2, y_pred2, average='weighted'))
+
+X_train5, X_test5, y_train5, y_test5 = train_test_split(processed_features_maj, labels, test_size=0.2, random_state=1, stratify=labels)
+cnb = ComplementNB()
+cnb.fit(X_train5, y_train5)
+y_pred5 = cnb.predict(X_test5)
+print(f1_score(y_test5, y_pred5, average='weighted'))
 
 #similar but using SVD data
 X_train3, X_test3, y_train3, y_test3 = train_test_split(processed_svd, labels, test_size=0.2, random_state=1, stratify=labels)
-text_classifier = RandomForestClassifier(n_estimators=200, random_state=0)
-text_classifier.fit(X_train3, y_train3)
-predictions = text_classifier.predict(X_test3)
+rf = RandomForestClassifier(n_estimators=200, random_state=0)
+rf.fit(X_train3, y_train3)
+predictions = rf.predict(X_test3)
 print(confusion_matrix(y_test3,predictions))
 print(classification_report(y_test3,predictions))
 print(accuracy_score(y_test3, predictions))
@@ -54,5 +60,4 @@ X_train4, X_test4, y_train4, y_test4 = train_test_split(processed_svd, labels, t
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(X_train4, y_train4)
 y_pred4 = svclassifier.predict(X_test4)
-
 print(f1_score(y_test4, y_pred4, average='weighted'))
